b1032dc53bbd2ee480a27dac59df4a0ae23fb914
Emplexx <ishchuk132@gmail.com>
2024-12-16 18:59:57 +0100

    Changed structure and tried to make this multiplaform (except the only target is jvm because there is not a single tutorial online about how to make a multiplatform library with no platform specific dependencies literally go figure)

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-	kotlin("jvm") version "2.1.0"
-	kotlin("plugin.serialization") version "2.1.0"
-}
--- wanikani/build.gradle.kts
+++ wanikani/build.gradle.kts
@@ -1,29 +1,43 @@
 plugins {
-	id("java-library")
-	kotlin("jvm")
-	kotlin("plugin.serialization")
+	kotlin("multiplatform") version "2.1.0"
+	kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "moe.emi.wanikani"
 version = "1.0-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+kotlin {
+	jvm()
+//	iosX64()
+//	iosArm64()
+//	iosSimulatorArm64()
+//	linuxX64()
+
+	sourceSets {
+		commonMain.dependencies {
+			
+				val ktor = "3.0.2"
+				implementation("io.ktor:ktor-client-core:$ktor")
+				implementation("io.ktor:ktor-client-content-negotiation:$ktor")
+				implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor")
+				implementation("io.ktor:ktor-client-cio:$ktor")
+				implementation("io.ktor:ktor-client-logging:$ktor")
+
+				implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+				implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
+		}
+		commonTest.dependencies {
+			implementation("ch.qos.logback:logback-classic:1.5.12")
+		}
+	}
 }
 
-dependencies {
-	
-	val ktor = "3.0.2"
-	implementation("io.ktor:ktor-client-core:$ktor")
-	implementation("io.ktor:ktor-client-content-negotiation:$ktor")
-	implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor")
-	implementation("io.ktor:ktor-client-cio:$ktor")
-	implementation("io.ktor:ktor-client-logging:$ktor")
-	
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-	
-//	implementation("ch.qos.logback:logback-classic:1.5.12")
-}
+//java {
+//	sourceCompatibility = JavaVersion.VERSION_17
+//	targetCompatibility = JavaVersion.VERSION_17
+//}
+
+//dependencies {
+////	implementation("ch.qos.logback:logback-classic:1.5.12")
+//}
 
